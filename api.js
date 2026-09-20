1d6eed1a4327043a2771284fbc81b3ebbc0320be
Isa <isabellealves4569@gmail.com>
2026-09-20 22:21:04 +0000

    ex2: lista de tarefas adicionada

--- api.js
+++ api.js
@@ -2,8 +2,18 @@ import express from 'express';
 
 const app = express();
 
+let tarefas = [
+    { id: 1, titulo: 'Escutar música', concluida: false },
+    { id: 2, titulo: 'Comer batata frita', concluida: true },
+    { id: 3, titulo: 'Dobrar roupas', concluida: false }
+];
+
 app.get('/', (req, res) => {
     res.send('API de Tarefas no ar');
+});
+
+app.get('/tarefas', (req, res) => {
+    res.json(tarefas);
 });
 
 app.listen(3000, () => {
